6958ac6c2f5479f0d6f5a83da099b78732945086
gfxgee <boygee2025@gmail.com>
2023-12-01 17:44:21 +0800

    new home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/students', Student::class)->name('student.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
